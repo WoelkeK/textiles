92ccf77a372a4d62bba0aa00661e4dd887aeae42
Krzysztof <krzysiek@woelke.pl>
2022-11-19 15:19:59 +0100

    init structure of textile app

---
+++ src/main/java/com/ambitious/team/textiles/TextilesApplication.java
@@ -0,0 +1,13 @@
+package com.ambitious.team.textiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextilesApplication.class, args);
+	}
+
+}
