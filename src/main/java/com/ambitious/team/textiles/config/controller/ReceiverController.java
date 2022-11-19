686e3e2e1d544a0bd0bfbf1a0e43eb919541bb36
Krzysztof <krzysiek@woelke.pl>
2022-11-19 18:21:09 +0100

    Add model, end-points api in controller, crud methods for service layer, and Jpa repository layer

--- src/main/java/com/ambitious/team/textiles/config/controller/ReceiverController.java
+++ src/main/java/com/ambitious/team/textiles/config/controller/ReceiverController.java
@@ -1,4 +1,66 @@
 package com.ambitious.team.textiles.config.controller;
 
+import com.ambitious.team.textiles.config.model.Receiver;
+import com.ambitious.team.textiles.config.service.ReceiverService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/receivers")
 public class ReceiverController {
+
+    private static final Logger LOGGER = Logger.getLogger(ReceiverController.class.getName());
+    private final ReceiverService receiverService;
+
+    public ReceiverController(ReceiverService receiverService) {
+        this.receiverService = receiverService;
+    }
+
+    @GetMapping
+    public List<Receiver> list() {
+        LOGGER.info("list()");
+        List<Receiver> receivers = receiverService.getList();
+        LOGGER.info("list(...) = " + receivers);
+        return receivers;
+    }
+
+    @PostMapping
+    public Receiver create(@RequestBody Receiver receiver) {
+        LOGGER.info("create()");
+        Receiver createdReceiver = receiverService.create(receiver);
+        LOGGER.info("create(...)");
+        return createdReceiver;
+    }
+
+    @GetMapping("/{id}")
+    public Receiver read(@PathVariable Long id) {
+        LOGGER.info("read(" + id + ")");
+        Receiver rededReceiver = receiverService.read(id);
+        LOGGER.info("read(...)");
+        return rededReceiver;
+    }
+
+    @PutMapping
+    public Receiver update(@RequestBody Receiver receiver) {
+        LOGGER.info("update()");
+        Receiver updatedReceiver = receiverService.update(receiver);
+        LOGGER.info("update(...)");
+        return updatedReceiver;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        LOGGER.info("delete()");
+        receiverService.delete(id);
+    }
 }
--- src/main/java/com/ambitious/team/textiles/config/service/ReceiverService.java
+++ src/main/java/com/ambitious/team/textiles/config/service/ReceiverService.java
@@ -1,4 +1,62 @@
 package com.ambitious.team.textiles.config.service;
 
+import com.ambitious.team.textiles.config.model.Receiver;
+import com.ambitious.team.textiles.config.repository.ReceiverRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@Service
+@RequiredArgsConstructor
 public class ReceiverService {
+
+    private static final Logger LOGGER = Logger.getLogger(ReceiverService.class.getName());
+    private final ReceiverRepository receiverRepository;
+
+    public List<Receiver> getList() {
+        LOGGER.info("getList()");
+
+        List<Receiver> receivers = receiverRepository.findAll();
+        LOGGER.info("getList(...)");
+        return receivers;
+    }
+
+    public Receiver create(Receiver receiver) {
+        LOGGER.info("create()");
+        Receiver createdReceiver = receiverRepository.save(receiver);
+        LOGGER.info("create(...)");
+        return createdReceiver;
+    }
+
+    public Receiver read(Long id) {
+        LOGGER.info("read(" + ")");
+        Optional<Receiver> receiver = receiverRepository.findById(id);
+        LOGGER.info("read(...)");
+        return receiver.orElseThrow();
+    }
+
+    public Receiver update(Receiver receiver) {
+        LOGGER.info("update()");
+        Receiver editableReceiver = receiverRepository.findById(receiver.getId()).orElseThrow();
+        editableReceiver.setReceiverId(receiver.getReceiverId());
+        editableReceiver.setId(receiver.getId());
+        editableReceiver.setReceiverId(receiver.getReceiverId());
+        editableReceiver.setName(receiver.getName());
+        editableReceiver.setAdrress(receiver.getAdrress());
+        editableReceiver.setCarbonFootprint(receiver.getCarbonFootprint());
+        editableReceiver.setTokens(receiver.getTokens());
+        Receiver updatedReceiver = receiverRepository.save(receiver);
+        LOGGER.info("update(...)");
+        return updatedReceiver;
+    }
+
+    public void delete(Long id) {
+        LOGGER.info("delete()");
+        Optional<Receiver> receiver = receiverRepository.findById(id);
+        receiverRepository.delete(receiver.orElseThrow());
+        LOGGER.info("delete(...)");
+    }
 }
